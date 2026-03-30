3bf9e98b7227962c7cb7454f296131ad4fb4fc3b
recalcitrantsupplant <dcchabgood@gmail.com>
2026-03-30 16:24:59 +1000

    Fix spatial CQL filters in demo and compiler

--- demo/app-static/app.js
+++ demo/app-static/app.js
@@ -6,7 +6,7 @@
 
 const CONFIG_PATH = 'config.ttl';
 const APP_CONFIG = window.APP_CONFIG || {};
-const FUSEKI_BASE = APP_CONFIG.fusekiBase || 'http://localhost:3030';
+const FUSEKI_BASE = APP_CONFIG.fusekiBase || 'http://localhost:3035';
 const RESULT_LIMITS = [10, 100, 1000, 5000, 9999];
 const DEFAULT_LIMIT = 10;
 const FACET_LIMITS = [10, 25, 50, 100, 500];
@@ -303,10 +303,11 @@ function extractConfig(store) {
                 sortable,
             });
 
+            if (fieldIRI) fieldIRIs[fieldName] = fieldIRI;
+
             if (facetable && !seenFacets.has(fieldName)) {
                 seenFacets.add(fieldName);
                 facetFields.push(fieldName);
-                if (fieldIRI) fieldIRIs[fieldName] = fieldIRI;
             }
 
             if (facetable && pathNode) {
